df26a417c96df3b639620b588b05794daaff1efe
CarlosAlvaIqui <carlos.alvarez.i@tecsup.edu.pe>
2018-11-18 15:54:00 -0500

    Laboratorios del 6 al 9

---
+++ lab07 y 08/GestorImagenes/resources/views/albumes/mostrar.blade.php
@@ -0,0 +1,39 @@
+@extends('app')
+
+@section('content')
+@if(Session::has('creado'))
+  <div class="alert alert-success">
+    <p>El album ha sido creado</p>
+  </div>
+@endif
+@if(Session::has('actualizado'))
+  <div class="alert alert-success">
+    <p>El album ha sido Actualizado</p>
+  </div>
+@endif
+
+<div class="container-fluid">
+  <p><a href="/validado/albumes/crear-album" class="btn btn-primary" role="button">Crear Álbum</a> </p>
+  @if(sizeof($albumes)>0)
+    @foreach($albumes as $album)
+    <div class="row">
+      <div class="col-sm-6 col-md-12">
+          <div class="thumbnail">
+              <div class="caption">
+                  <h3>{{$album->nombre}}</h3>
+                  <p>{{$album->descripcion}}</p>
+                  <p><a href="/validado/fotos?id={{$album->id}}" class="btn btn-primary" role="button">
+                  Ver Fotos</a> </p>
+                  <p> <a href="/validado/albumes/actualizar-album/{{$album->id}}" class="btn btn-primary" role="button">Editar Album</a></p>
+              </div>
+          </div>
+      </div>
+    </div>
+    @endforeach
+  @else
+  <div class="alert alert-danger">
+    <p>Al parecer no tienen ningun album. cree UNO!!</p>
+  </div>
+  @endif
+</div>
+@endsection
